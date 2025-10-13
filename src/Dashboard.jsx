71d44ea7fb746b78a127cb237d0dd6994cc10661
Akshaya Mishra <113210067+akshaya1119@users.noreply.github.com>
2025-10-13 14:09:26 +0530

    prevent overflowing of text on project cards

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -94,7 +94,7 @@ export default function Dashboard() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-3 gap-6">
         <div className="md:col-span-2">
           <h3 className="text-2xl font-semibold text-gray-800 mb-4">Select a Project</h3>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
@@ -104,7 +104,9 @@ export default function Dashboard() {
                 className="bg-white p-6 rounded-lg shadow-md border-l-4 border-gray-400 hover:shadow-xl hover:bg-gray-50 transition-all duration-300 cursor-pointer"
                 onClick={() => handleCardClick(project.id, project.name)}
               >
-                <h3 className="text-gray-800 text-lg font-semibold mb-2">{project.name}</h3>
+                <h3 className="text-gray-800 text-lg font-semibold mb-2 truncate" 
+                title={project.name}
+                >{project.name}</h3>
                 <p className="text-sm text-gray-500">Last accessed: 2 hours ago</p>
               </div>
             ))}
